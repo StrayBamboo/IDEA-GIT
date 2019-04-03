2066f139314b5638d277c84065bac04fdf2ec1c1
mitu <1176756343@qq.com>
2019-04-03 22:48:52 +0800

    2222

---
+++ src/com/itheima/demo/Demo.java
@@ -0,0 +1,9 @@
+package com.itheima.demo;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("shot");
+        System.out.println("shot");
+        System.out.println("shot");
+    }
+}
